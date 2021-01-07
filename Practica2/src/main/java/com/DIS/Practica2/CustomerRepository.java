b981946ccc8c4b7841cba39cea9992bd5dcca93c
marcos rodriguez <peter99@example.com>
2021-01-07 18:39:50 +0100

    Primera Ejecución

---
+++ Practica2/src/main/java/com/DIS/Practica2/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.DIS.Practica2;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    List<Customer> findByLastNameStartsWithIgnoreCase(String lastName);
+}
